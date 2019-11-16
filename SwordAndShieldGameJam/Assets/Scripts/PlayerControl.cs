9f794f95512a6915a3c0cd39570b3724f50e6381
AlphaAndrew <54077501+AlphaAndrew@users.noreply.github.com>
2019-11-15 19:33:06 -0500

    Base player controls

--- SwordAndShieldGameJam/Assets/Scripts/PlayerControl.cs
+++ SwordAndShieldGameJam/Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,10 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody playerRB;
     private GameObject player;
     public float playerSpeed;
+    public float chargeMultiplier;
+    private float chargeTimer;
+    public float chargeLimit;
+    //public Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +23,55 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        MovementControls();
+        //Normal Attack
+        //Charge Attack
+        if (Input.GetMouseButton(0))
+        {
+            if (chargeTimer < chargeLimit)
+            {
+                chargeTimer += Time.deltaTime;
+            }
+        }
+        else if(Input.GetMouseButtonUp(0)) {
+            Debug.Log("Release");
+            //Attack
+            ChargeAttack();
+            chargeTimer = 0;
+        }
+    }
+
+    public void ChargeAttack()
+    {
+
+        player.transform.position += transform.forward * (chargeMultiplier * chargeTimer); //* (chargeTimer/120);
+    }
+    public void MovementControls()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
             //playerRB.velocity = transform.right * playerSpeed;
             player.transform.position += transform.right * playerSpeed;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             //playerRB.velocity = -transform.right * playerSpeed;
             player.transform.position += -transform.right * playerSpeed;
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            //playerRB.velocity = -transform.right * playerSpeed;
+            player.transform.position += transform.forward * playerSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //playerRB.velocity = -transform.right * playerSpeed;
+            player.transform.position += -transform.forward * playerSpeed;
+        }
+
+        //float x = Input.GetAxis("Mouse Y");
+        float y = -Input.GetAxis("Mouse X");
+        player.transform.Rotate(0, -y, 0);
+        //Camera.main.transform.Rotate(-x, 0, 0);
     }
 }
