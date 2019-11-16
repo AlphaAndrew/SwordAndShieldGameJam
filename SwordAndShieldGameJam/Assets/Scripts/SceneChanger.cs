37c715c871bc7de85671f6ad3ef7e0656dcfd96f
AlphaAndrew <54077501+AlphaAndrew@users.noreply.github.com>
2019-11-16 13:23:30 -0500

    Scene Management

---
+++ SwordAndShieldGameJam/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChanger : MonoBehaviour
+{
+    public string levelOne;
+
+    public void LevelOne()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
