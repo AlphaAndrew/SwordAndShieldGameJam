7ef17c22996ba23d57dcd76cd9ec3d52b4fd871e
BrandonCoulombe <54905202+BrandonCoulombe@users.noreply.github.com>
2019-11-15 21:05:40 -0500

    Sword

---
+++ SwordAndShieldGameJam/Assets/Scripts/CapturePoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> playersInRange;
+    void Start()
+    {
+        playersInRange = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SwordAndShieldGameJam/Assets/Scripts/Sword.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    PlayerControl playerScript;
+
+    private void Start()
+    {
+        playerScript = GetComponentInParent<PlayerControl>();
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string colliderTag = other.gameObject.tag;
+
+        switch (colliderTag)
+        {
+            case "Player":
+                //check if regular or charged attack with bool
+                if (playerScript.isCharging)
+                {
+                    //apply big dam, instant kill
+                }
+                else //normal attack
+                {
+                    //apply damage
+                }
+
+                break;
+
+            case "Shield":
+                // check if regular or charged attack with bool
+                if (playerScript.isCharging)
+                {
+                    //bounce back mechanic
+                }
+                else //normal attack
+                {
+                    //blocked, nothin happens?
+                }
+                break;
+
+            default:
+                Debug.Log("Collided with something not in switch statemetn");
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+}
