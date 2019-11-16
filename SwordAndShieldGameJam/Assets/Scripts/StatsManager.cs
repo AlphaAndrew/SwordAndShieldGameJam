62ade1dc95585018570cc32ef030c3df774e76c5
BrandonCoulombe <54905202+BrandonCoulombe@users.noreply.github.com>
2019-11-15 22:14:07 -0500

    Update StatsManager.cs

--- SwordAndShieldGameJam/Assets/Scripts/StatsManager.cs
+++ SwordAndShieldGameJam/Assets/Scripts/StatsManager.cs
@@ -5,13 +5,30 @@ using UnityEngine.Networking;
 
 public class StatsManager : NetworkBehaviour
 {
-
+    public int redTeamScore;
+    public int blueTeamScore;
+    public GameObject[] players;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (isServer)
+        {
+            FillPlayerList();
+            GetScores();
+        }
 
+    }
+    public void FillPlayerList()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+    public void GetScores()
+    {
+        foreach (GameObject player in players)
+        {
+            
+        }
+    }
     // Update is called once per frame
     void Update()
     {
