3e55a529f62043ca2b412337768f0595eeef4486
AlphaAndrew <54077501+AlphaAndrew@users.noreply.github.com>
2019-11-16 16:10:41 -0500

    Ui work

---
+++ SwordAndShieldGameJam/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class LobbyManager : NetworkLobbyManager
+{
+    public GameObject lobby;
+
+    private void Start()
+    {
+        lobby.SetActive(false);
+    }
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        lobby.SetActive(true);
+        Debug.Log("Game Start");
+    }
+
+}
--- SwordAndShieldGameJam/Assets/Scripts/LobbyScript.cs
+++ SwordAndShieldGameJam/Assets/Scripts/LobbyScript.cs
@@ -5,5 +5,11 @@ using UnityEngine.Networking;
 
 public class LobbyScript : NetworkLobbyPlayer
 {
-
+    public GameObject parentPref;
+    public override void OnClientEnterLobby()
+    {
+        base.OnClientEnterLobby();
+        parentPref = GameObject.FindGameObjectWithTag("ParentPref");
+        gameObject.transform.SetParent(parentPref.transform);
+    }
 }
