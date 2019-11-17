170c572416f521a84b68a8b864c8000a3b61ebc3
BrandonCoulombe <54905202+BrandonCoulombe@users.noreply.github.com>
2019-11-17 17:48:41 -0500

    ads

---
+++ SwordAndShieldGameJam/Assets/Boundary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    public Transform resetPoint;
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerObject")
+        {
+            other.gameObject.transform.position = resetPoint.position;
+        }
+    }
+}
